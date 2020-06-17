e4aee3702ff8d9739747aafd578ff1b349bf88a1
Pedro Muzy <pedro.geovani.paulino@gmail.com>
2020-06-17 12:29:29 -0300

    + página do jogo

--- app/App.tsx
+++ app/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Jogo from './src/pages/jogo';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>BIG JOGO DA VELHA</Text>
-    </View>
+    <>
+      <Jogo/>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
---
+++ app/src/pages/jogo/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, ImageBackground, Image, StyleSheet, StatusBar } from 'react-native';
+
+const Jogo = () => {
+  return <View />
+};
+
+export default Jogo;
